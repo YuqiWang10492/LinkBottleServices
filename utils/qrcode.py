ad62cb021694d267e938359b09413ff52f799c52
YuqiWang10492 <yuqiwangcontact@gmail.com>
2025-12-16 04:26:38 -0500

    account linking and AWS S3 storage of QR code

--- utils/qrcode.py
+++ utils/qrcode.py
@@ -1,5 +1,19 @@
 from io import BytesIO
 import qrcode
+import boto3
+import os
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-2")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME", "linkbottle-bucket")
+
+s3 = boto3.client(
+    "s3",
+    region_name=AWS_REGION,
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+)
 
 def generate_qr_code(data: str) -> BytesIO:
     """
@@ -19,3 +33,16 @@ def generate_qr_code(data: str) -> BytesIO:
     img.save(byte_io, format='PNG')
     byte_io.seek(0)
     return byte_io
+
+def upload_qr_to_s3(key: str, png_bytes: bytes) -> str:
+    s3_key = f"qr/{key}.png"
+
+    s3.put_object(
+        Bucket=AWS_BUCKET_NAME,
+        Key=s3_key,
+        Body=png_bytes,
+        ContentType="image/png",
+    )
+
+    # Return the public URL
+    return f"https://{AWS_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{s3_key}"
